2141d576d6df85290164cf011957e50157899501
Jennifer Davila <pazjenni1331@gmail.com>
2021-07-14 20:08:46 -0500

    Updated index.js file to run inquirer prompts and generate a new html page with templates updated in the template page

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
+const generateTeam = require('./src/page-template');
 
-function generateEmployee() {
+var allEmployees = [];
+
+function generateInfo() {
     inquirer.prompt ([
+        {
+            name: "team",
+            type: "input",
+            message: "What is your team name?",
+            validate: (input) => {
+                if(input) {
+                    return true;
+                } else {
+                    return "Please insert a valid input."
+                }
+            }
+        },
         {
             name: "name",
             type: "input",
@@ -55,8 +70,86 @@ function generateEmployee() {
                 "Engineer",
                 "Intern",
             ]
-        },
+        }
     ])
+
+        .then((response) => {
+            switch(response.Role) {
+                case 'Manager':
+                    prompt(
+                        {
+                            name: "OfficeNumber",
+                            type: "input",
+                            message: "What is the Manager's office number?",
+                            validate: (input) => {
+                                if(input){
+                                    return true;
+                                } else {
+                                    return "Please insert a valid input."
+                                }
+                            }
+                        },
+                    ).then((managerResponse) =>{
+                        managerResponse.OfficeNumber
+                        let newManager = new Manager (response.name, response.Id, response.Email, managerResponse.OfficeNumber) //by using the manager class, creating a new manager with parameters
+                        allEmployees.push(newManager); //pushes new manager to the all employees array
+                    })
+                    break;
+
+                case 'Engineer':
+                    prompt(
+                        {
+                            name: "Github",
+                            type: "input",
+                            message: "What is the Engineer's gitub URL?",
+                            validate: (input) => {
+                                if(input){
+                                    return true;
+                                } else {
+                                    return "Please insert a valid input."
+                                }
+                            }
+                        },
+                    ).then((engineerResponse) =>{
+                        engineerResponse.Github
+                        let newEngineer = new Engineer (response.name, response.Id, response.Email, engineerResponse.Github) //with the engineer class, creating a new engineer with parameters set in class
+                        allEmployees.push(newEngineer); //pushes new engineer to the all employees array
+                    })
+                    break;
+
+                case 'Intern':
+                    prompt(
+                        {
+                            name: "School",
+                            type: "input",
+                            message: "What school does the Intern attend?",
+                            validate: (input) => {
+                                if(input){
+                                    return true;
+                                } else {
+                                    return "Please insert a valid input."
+                                }
+                            }
+                        },
+                    ).then((internResponse) =>{
+                        internResponse.School
+                        let newIntern = new Intern (response.name, response.Id, response.Email, internResponse.School) //with the intern class, creating a new intern with parameters set in class
+                        allEmployees.push(newIntern); //pushes new intern to the all employees array
+                    })
+                    break;
+            }
+           
+        }).then(() => {
+            let htmlcontent = generateTeam(allEmployees)  //when calls function will pass in the array of all employees
+            generatePage(response.team, htmlcontent); //taking in the strings
+        })
+  
 }
 
+generateInfo();
 
+function generatePage(fileName, htmlcontent) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.html`, htmlcontent, (err)=> {
+        err ? console.error(err) : console.log('Success!')
+   });
+}
--- src/page-template.js
+++ src/page-template.js
@@ -1,22 +1,122 @@
-const managerTemplate = ``
-
-const internTemplate = ``
-
-const engineerTemplate = ``
-
-const employeeTemplate = ``
-
-
-//create an if statement that pushes to the array once confirmed if a manager, intern, engineer, or just an employee
-const team = [];
-
-if( `${response.role === 'manager'}`) {
-    return generateManager().push(team);
-} else if(`${response.role}` === 'engineer') {
-    return generateEngineer().push(team);
-} else if (`${response.role}` === 'intern') {
-    return generateIntern().push(team);
-} else if (`${response.role}` === 'employee') {
-    return generateEmployee().push(team);
+function managerTemplate (manager) { 
+    return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${manager.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${manager.role}</h6>
+    <p class="card-text">Employee Id: ${manager.id}</p>
+    <p class="card-text">Office number: ${manager.officeNumber}</p>
+    <p class="card-text">Email: ${manager.email}</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+`
 }
 
+function internTemplate (intern) {
+ return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${intern.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${intern.role}</h6>
+    <p class="card-text">Employee Id: ${intern.id}</p>
+    <p class="card-text">School: ${intern.school}</p>
+    <p class="card-text">Email: ${intern.email}</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+`
+}
+
+function engineerTemplate (engineer) { 
+    return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${engineer.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${engineer.role}</h6>
+    <p class="card-text">Employee Id: ${engineer.id}</p>
+    <p class="card-text">Github URL: ${engineer.github}</p>
+    <p class="card-text">Email: ${engineer.email}</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`
+}
+
+function employeeTemplate (employee) {
+return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${employee.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${employee.role}</h6>
+    <p class="card-text">Employee Id: ${employee.id}</p>
+    <p class="card-text">Email: ${employee.email}</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+`}
+
+//depending on the role the user chooses in the inquirer prompt's, the switch statement will choose the correct template to input into the HTML template
+
+function renderEmployee(employee) {
+switch (employee.role) {
+    case 'Employee':
+        return employeeTemplate(employee);
+        break;
+    case 'Manager':
+        return managerTemplate(employee);
+        break;
+    case 'Intern':
+        return internTemplate(employee);
+        break;
+    case 'Engineer':
+        return engineerTemplate(employee);
+        break;
+};
+};
+
+//page template for the cards to display onto
+function htmlTemplate(team) { 
+    return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Employee Directory</title>
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./dist/style.css"/>
+  </head>
+
+  <body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Employee Directory</h1>
+    <p class="lead">Please select an employee below.</p>
+  </div>
+</div>
+ ${team}
+
+</body>`
+}
+
+//create an if statement that pushes to the array once confirmed if a manager, intern, engineer, or just an employee
+
+function generateTeam(allEmployees) {
+    let team = []; //empty array to pass generated team templates into
+
+    allEmployees.forEach(employee => {
+        let template = renderEmployee(employee); //is rendering the employee template into a variable
+        team.push(template); //will push the template variable into the team array
+    });
+
+    let teamTemplate = htmlTemplate(team); //returns string that has htmlcode in it
+    console.log(teamTemplate);
+    return teamTemplate; //gets value from the function
+}
+
+
+
+
+
+module.exports = generateTeam;
